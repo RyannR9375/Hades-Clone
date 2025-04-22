68111bf28d90e3fa1fdf21ccc9d62c2ca91368c6
Ryan R <33332787+RyannR9375@users.noreply.github.com>
2025-04-21 17:05:11 -0700

    Fixed user drag error

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -89,8 +89,8 @@ public class Player : MonoBehaviour, IAttackable
         _move = data.ReadValue<Vector2>();
         Vector3 movement = new Vector3(_move.x, 0f, _move.y);
         UnityEngine.Vector3 moveVector = Speed * Time.fixedDeltaTime * Time.timeScale * transform.TransformDirection(_move);
-        _rb.linearVelocity = new UnityEngine.Vector3(moveVector.x, _rb.linearVelocity.y, moveVector.y);
-        //_rb.linearVelocity = new UnityEngine.Vector3(moveVector.x, 0, moveVector.y);
+        _rb.linearVelocity = new UnityEngine.Vector3(moveVector.x, 0, moveVector.y);
+        //_rb.linearVelocity = new UnityEngine.Vector3(moveVector.x, _rb.linearVelocity.y, moveVector.y);
     }
 
     void MakeSingleton()
